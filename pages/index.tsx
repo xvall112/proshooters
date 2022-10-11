de5501a14e7c032512a5f60f334cec23485dc73e
xvall112 <valtalukas@seznam.cz>
2022-10-11 14:50:01 +0200

    proshooters

---
+++ pages/index.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import type { NextPage } from "next";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "../src/Link";
+import ProTip from "../src/ProTip";
+import Copyright from "../src/Copyright";
+import client from "../src/utils/ApolloClient";
+import GET_CATEGORIES_QUERY from "../src/utils/gql/queries/get-categories";
+
+const Home: NextPage = (props) => {
+  console.log(props);
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          MUI v5 + Next.js with TypeScript example
+        </Typography>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+};
+export default Home;
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: GET_CATEGORIES_QUERY,
+  });
+
+  return {
+    props: {
+      productCategories: data?.productCategories?.nodes || [],
+    },
+    revalidate: 1,
+  };
+}
